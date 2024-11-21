6ec420ec4b1525c43cf28d58d784994177f708f0
Edwin Yassunaga <yassunagajun@gmail.com>
2024-11-21 14:33:11 -0300

    Inicio do Projeto ABCEL

---
+++ src/main/java/com/abcelsystem/exposicao/ExposicaoAgricolaAbcelApplication.java
@@ -0,0 +1,13 @@
+package com.abcelsystem.exposicao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExposicaoAgricolaAbcelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExposicaoAgricolaAbcelApplication.class, args);
+	}
+
+}
